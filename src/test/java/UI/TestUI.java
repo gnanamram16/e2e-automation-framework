803eae2fd2e6cd8454510ace8b70513edd85b50f
gnanamram16 <gnanamram16@gmail.com>
2026-04-07 10:36:00 +0530

    Fixed CI failure - enabled headless mode

--- src/test/java/UI/TestUI.java
+++ src/test/java/UI/TestUI.java
@@ -11,7 +11,7 @@ public class TestUI {
         try (Playwright playwright = Playwright.create()) {
 
             Browser browser = playwright.chromium().launch(
-                    new BrowserType.LaunchOptions().setHeadless(false)
+                    new BrowserType.LaunchOptions().setHeadless(true)
             );
 
             Page page = browser.newPage();
